da12c761b130a864080b90a03815df71887298d7
User <zasbox@gmail.com>
2024-01-27 15:14:15 +0500

    homework 19.2

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,14 +1,13 @@
 from django.urls import path
 
 from catalog.apps import CatalogConfig
-from catalog.views import index, contacts, product_list, product_detail
+from catalog.views import contacts, ProductListView, ProductDetailView, ProductCreateView
 
 app_name = CatalogConfig.name
 
 urlpatterns = [
-    path('', index, name='index'),
+    path('', ProductListView.as_view(), name='index'),
+    path('products/<int:pk>/', ProductDetailView.as_view(), name='product_detail'),
+    path('products/create/', ProductCreateView.as_view(), name='create_product'),
     path('contacts/', contacts, name='contacts'),
-    path('products/<int:pk>/', product_detail, name='product_detail'),
-    path('products/', product_list, name='product_list'),
-    path('products/pages/<int:page>/', product_list, name='product_list'),
 ]
--- catalog/views.py
+++ catalog/views.py
@@ -1,62 +1,36 @@
-from django.core.files.storage import FileSystemStorage
+from django.http import HttpResponse
 from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views.generic import ListView, DetailView, CreateView
 
-from catalog.forms import ProductForm
-from catalog.models import Product, Category
-from config import settings
+from catalog.models import Product
 
 
 # Create your views here.
 
-def index(request):
-    # if request.method == 'POST':
-    #     name = request.POST.get('name')
-    #     phone = request.POST.get('phone')
-    #     message = request.POST.get('message')
-    #     print(f'{name}, {phone}, {message}')
-    return product_list(request, 1)
-    # product_list = Product.objects.all()
-    # context = {
-    #     'object_list': product_list,
-    #     'page_list': range(1, product_list.count() // 2 + 1 + bool(product_list.count() % 2))
-    # }
-    # return render(request, 'catalog/home.html', context)
+class ProductListView(ListView):
+    model = Product
+    paginate_by = 2
+    template_name = "catalog/home.html"
+
+
+class ProductDetailView(DetailView):
+    model = Product
+    template_name = "catalog/products.html"
+
+
+class ProductCreateView(CreateView):
+    model = Product
+    fields = '__all__'
+    success_url = reverse_lazy('catalog:index')
+
+    def form_invalid(self, form):
+        return HttpResponse('Unsuccess')
 
 
 def contacts(request):
     return render(request, 'catalog/contacts.html')
 
 
-def product_list(request, page=None):
-    if request.method == 'POST':
-        form = ProductForm(request.POST, request.FILES)
-        if form.is_valid():
-            form.save()
-        product_list = Product.objects.all()
-        context = {
-            'object_list': product_list,
-        }
-        return render(request, 'catalog/home.html', context)
-    elif page is not None:
-        begin = 2 * (page - 1)
-        product_list = Product.objects.all()
-        context = {
-            'object_list': product_list[begin:begin + 2],
-            'page_list': range(1, product_list.count() // 2 + 1 + bool(product_list.count() % 2))
-        }
-        return render(request, 'catalog/home.html', context)
-
-    else:
-        begin = 0
-        if page is not None:
-            begin = 2 * (page - 1) - 1
-        category_list = Category.objects.all()[begin:]
-        context = {
-            'object_list': category_list,
-        }
-        return render(request, 'catalog/add_product.html', context)
 
 
-def product_detail(request, pk):
-    product = Product.objects.get(pk=pk)
-    return render(request, 'catalog/products.html', {'object': product})
